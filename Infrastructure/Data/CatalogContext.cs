06b66f243db1a70737ce5b196f64addcbd79531d
dtk77 <vasilenko.andrey77@outlook.com>
2022-01-31 01:17:21 +0700

    Infrastructure section added, EntityFramework added, DBContext created

---
+++ Infrastructure/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using ApplicationCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class CatalogContext : DbContext
+    {
+        public CatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\\MSSQLLocalDB;Database=CatalogVGames;Trusted_Connection=True;");
+        }
+    }
+}
