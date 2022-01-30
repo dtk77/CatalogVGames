1e2bb84243db81cdce08fc9367942b7a3c3f572e
dtk77 <vasilenko.andrey77@outlook.com>
2022-01-31 01:12:11 +0700

    Added catalog model.

---
+++ ApplicationCore/Entities/Game.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ApplicationCore
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Developer { get; set; }
+
+
+
+        public virtual IList<Genre> Genre { get; set; } = new List<Genre>();
+    }
+}
---
+++ ApplicationCore/Entities/Genre.cs
@@ -0,0 +1,12 @@
+﻿namespace ApplicationCore
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+
+        public IList<Game> Games { get; set; } = new List<Game>();
+    }
+}
